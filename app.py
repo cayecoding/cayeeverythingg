3255f50ab20cae87b676bf690de9a771379ef30f
cayecoding <152365759+cayecoding@users.noreply.github.com>
2023-12-01 14:20:58 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,9 @@
 import streamlit as st
-st.write('Hello World')
+
+#https://www.webfx.com/tools/emoji-cheat-sheet/
+st.set_page_config(page_title="My Webpage", page_icon=":tada:", layout="wide")
+# ---- HEADER SECTION ----
+st.subheader("Hi, I am Caye Guadalou L. Ruaya :wave:")
+st.title("The Journey Of My Life")
+st.write("Life As an Engineering Student, Let's go!")
+st.write("{Curious About My Life or Who Am I, Check it out my FB Acc}(https://www.facebook.com/eyacc31/)")
