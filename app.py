0d1fb31a67a73254a0f2ac30c49f750e152942b9
cayecoding <152365759+cayecoding@users.noreply.github.com>
2023-12-04 14:19:51 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ st.set_page_config(page_title="My Webpage", page_icon=":tada:", layout="wide")
 # ---- HEADER SECTION ----
 with st.container():
     st.subheader("Hi, I am Caye Guadalou L. Ruaya :wave:")
-    st.title("The Journey Of My Life")
+    st.title("All About My Life")
     st.write("Life As an Engineering Student, Let's go!")
-    st.write("{Curious About My Life or Who Am I?, Check it out my FB Acc}(https://www.facebook.com/eyacc31/)")
+    st.write("Curious About My Life or Who Am I?, Check it out my FB Acc(https://www.facebook.com/eyacc31/)")
 
 # ---- Life Background ----
 with st.container():
